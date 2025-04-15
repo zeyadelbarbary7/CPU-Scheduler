50c27edbe085d194fdc293ad8142a803d48e72a7
Zeyad Elbarbary <167577132+zeyadelbarbary7@users.noreply.github.com>
2025-04-15 03:28:19 +0200

    Non Preemptive Priority (completed)

--- Priority.cpp
+++ Priority.cpp
@@ -1,5 +1,5 @@
 #include "Priority.h"
-
+#include <algorithm>
 
 void priority(){
     vector <Process>processes;
@@ -9,58 +9,54 @@ void priority(){
     cout<<"enter number of processes"<<endl;
     cin>>n;
     for (int i = 0; i < n; i++){
-        cin>>name>>burstTime>>arrivalTime>>priority;
+        name = "P" + to_string(i+1);
+        cout<<"enter arrival time for "<<name<<endl;
+        cin>>arrivalTime;
+        cout<<"enter burst time for "<<name<<endl;
+        cin>>burstTime;
+        cout<<"enter priority for "<<name<<endl;
+        cin>>priority;
         Process process = Process(name, burstTime, arrivalTime, priority);
         processes.push_back(process);
-        cout<<"gwa awel for";
     }
     cout<<endl;
-    cout<<"abl hesab el total burst time"<<endl;
     int totalBurstTime=0;
     for (int i = 0; i < processes.size(); i++){
-        totalBurstTime+=processes[i].getBurstTime();       //total processes time
-        cout<<"bn7sb";
+        totalBurstTime+=processes[i].getBurstTime();
     }
     cout<<endl;
-    cout<<totalBurstTime<<endl;
     int time =0;
+    cout<<"--- Priority Scheduling Result ---"<<endl;
+    cout<<"process\tArrival\tBurst\tPriority\tstart\tend\twaiting"<<endl;
     
-    int minIndex = 0;
-    int minArrival = processes[0].getArrivalTime();
+
     vector<Process> minProcesses;
-    // minProcesses.push_back(processes[0]); 
+    int totalWaitingTime=0,waitingTime=0;
     do{
-        cout<<"gwa el do"<<endl;
         for (int i = 0; i < processes.size(); ){
             if (processes[i].getArrivalTime()<=time){
                 minProcesses.push_back(processes[i]);
                 processes.erase(processes.begin()+i);
-                cout<<"readyProcesses";
             }else{
                 i++;
             }   
         }
+        sort(minProcesses.begin(), minProcesses.end(), []( Process &p1,  Process &p2) {
+            return p1.getPriority() < p2.getPriority(); // Ascending order of arrival time
+        });
+        cout<<minProcesses[0].getName()<<"\t"<<minProcesses[0].getArrivalTime()<<"\t"<<minProcesses[0].getBurstTime()<<"\t"<<minProcesses[0].getPriority()<<"\t\t"<<time<<"\t";
+        time+=minProcesses[0].getBurstTime();
+        waitingTime = time-minProcesses[0].getArrivalTime()-minProcesses[0].getBurstTime();
+        cout<<time<<"\t"<<waitingTime<<endl;
+        minProcesses.erase(minProcesses.begin()); // remove the process that has been executed
 
-        cout<<endl;
-        // vector<Process> minPriority;
-        int higherPriorityIndex=0;
-        Process currentProcess;
+        totalWaitingTime+=time-minProcesses[0].getArrivalTime()-minProcesses[0].getBurstTime();
+        }while (time<totalBurstTime);
 
-        if(!minProcesses.empty()){
-            for (int i = 0; i < minProcesses.size(); i++){
-                if (minProcesses[i].getPriority() < minProcesses[higherPriorityIndex].getPriority()){
-                    higherPriorityIndex = i;
-                    currentProcess = minProcesses[i];
-                }
-            }
-            minProcesses.erase(minProcesses.begin() + higherPriorityIndex);
-            time += currentProcess.getBurstTime();
-            cout<<currentProcess.getName()<<endl;
-            
-        }
-        }while (time < totalBurstTime);
-
-    
+        float averageWaitingTime = (float)totalWaitingTime/n;
+        float averageTurnAroundTime = (float)(totalWaitingTime+totalBurstTime)/n;
+        cout<<"Average Waiting Time : "<<averageWaitingTime<<endl;
+        cout<<"Average Turnaround Time : "<<averageTurnAroundTime<<endl;
 }
 
 //testtttt
